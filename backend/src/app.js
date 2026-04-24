32931af223e0992445600a16f5b9394c7cd5a61d
Mohan Kumar Dalei <mohankumardalei2001@gmail.com>
2026-04-25 00:15:06 +0530

    sraha-ai-setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,14 +11,14 @@ const app = express();
 
 // 👇 YAHAN CHANGE KIYA HAI
 // Frontend ka static folder (Build ke baad)
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use(cors({
     origin: "http://localhost:5173", // Apna React/Vite ka exact URL dalein
     credentials: true // Cookies allow karne ke liye ye zaroori hai
 }));
 // SPA fallback
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 app.use(express.json());
 app.use(cookieParser());
